91699daf71d655aae2eb1b9048716365c32e6d69
Shirin N. Monfared <109618581+SheenNaMon@users.noreply.github.com>
2023-04-13 12:10:07 -0400

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Sheen',
+    age: 29,
+    image:
+      'https://i.pinimg.com/236x/c0/69/58/c06958034a2a96f98a8f7f91663b3ba3.jpg',
+  },
+  {
+    id: 2,
+    name: 'Shayan',
+    age: 32,
+    image:
+      'https://images.unsplash.com/photo-1602249586608-38844047ea51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 3,
+    name: 'Peter',
+    age: 36,
+    image:
+      'https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://images.unsplash.com/photo-1592993497128-319c051e90cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTIxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  },
+];
